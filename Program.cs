ae8bcb16ae18cf24285d020e51bc0e38f57e654c
Tiffany Siu <tsiu88@gmail.com>
2020-02-28 11:47:18 -0800

    Add reading input and checking input type

--- Program.cs
+++ Program.cs
@@ -32,6 +32,25 @@ class Program
   {
     Console.WriteLine("======= Your Order =======");
     Console.WriteLine("How many loaves of bread would you like?");
+    string breadString = Console.ReadLine();
+    int breadQuantity = CheckInput(breadString);
     Console.WriteLine("How many pastries would you like?");
+    string pastryString = Console.ReadLine();
+    int pastryQuantity = CheckInput(pastryString);
+  }
+
+  static int CheckInput(string input)
+  {
+    int number;
+    bool isInt = Int32.TryParse(input, out number);
+    if (isInt)
+    {
+      return number;
+    }
+    else
+    {
+      Console.WriteLine("Please enter a number.");
+      return -1;
+    }
   }
 }
